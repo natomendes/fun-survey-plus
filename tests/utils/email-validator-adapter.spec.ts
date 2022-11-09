81ee1bd627bb46d64d2b352bc843e31946428675
Renato Mendes <mendeslrenato@gmail.com>
2022-11-09 19:40:57 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ tests/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from '../../src/utils/email-validator-adapter'
+
+describe('EmailValidator Adapter', () => {
+  it('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
