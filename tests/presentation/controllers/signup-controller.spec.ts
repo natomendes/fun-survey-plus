0983c1a3af5fc353587a294cbb068ca1a33d9735
Renato Mendes <mendeslrenato@gmail.com>
2022-11-09 18:11:34 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- tests/presentation/controllers/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -166,6 +166,25 @@ describe('SignUpController', () => {
     expect(httpResponse?.body).toEqual(new ServerError())
   })
 
+  it('Should return 500 if EmailValidator throws', () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add')
+      .mockImplementationOnce(() => {
+        throw new Error()
+      })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(500)
+    expect(httpResponse?.body).toEqual(new ServerError())
+  })
+
   it('Should call AddAccount with correct values', () => {
     const { sut, addAccountStub } = makeSut()
     const addSpy = jest.spyOn(addAccountStub, 'add')
