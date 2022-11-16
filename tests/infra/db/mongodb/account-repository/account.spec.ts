3db737f76d782a5445756df11098b1854c1989fc
Renato Mendes <mendeslrenato@gmail.com>
2022-11-15 21:55:32 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ tests/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,22 @@
+import { AccountMongoRepository } from '../../../../../src/infra/db/mongodb/account-repository/account'
+import { MongoHelper } from '../../../../../src/infra/db/mongodb/helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const isValid = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(isValid).toBe(true)
+  })
+})
