a7bb8b85d8d4d8deb3015f585bab9da04ea8dc48
Renato Mendes <mendeslrenato@gmail.com>
2022-11-16 23:20:52 -0300

    feat: ensure requests parse body content as json

---
+++ tests/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../../../src/main/config/app'
+
+describe('Body Parser Middleware', () => {
+  it('Should parse body content as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'any_name' })
+      .expect({ name: 'any_name' })
+  })
+})
