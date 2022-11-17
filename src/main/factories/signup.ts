5e23b5f0fe370e43f15c24c745cca656c23dccf7
Renato Mendes <mendeslrenato@gmail.com>
2022-11-17 01:05:16 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup-controller'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+
+  const emailValidator = new EmailValidatorAdapter()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+
+  return new SignUpController(emailValidator, dbAddAccount)
+}
