5a496218f442f71acd5ec4b81e0dfba17d3ef42c
Renato Mendes <mendeslrenato@gmail.com>
2022-12-07 13:52:56 -0300

    feat: ensure LogMongoRepository createas an error log on success

---
+++ src/infra/db/mongodb/log-repository.ts
@@ -0,0 +1,12 @@
+import { LogErrorRepository } from '../../../data/protocols'
+import { MongoHelper } from './helpers/mongo-helper'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    const errorCollection = MongoHelper.getCollection('errors')
+    await errorCollection.insertOne({
+      stack,
+      date: new Date()
+    })
+  }
+}
