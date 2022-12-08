34d5d18641a9031f11b9f4dae8af68ce874023b2
Renato Mendes <mendeslrenato@gmail.com>
2022-12-08 17:31:46 -0300

    refactor: move sut creation to a factory method and add faker to test

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -1,30 +1,37 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from '@/infra/criptography/jwt-adapter'
+import { faker } from '@faker-js/faker'
+
+const makeSut = (secret = faker.datatype.uuid()): JwtAdapter => {
+  return new JwtAdapter(secret)
+}
 
 describe('Jwt Adapter', () => {
   it('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const secret = faker.datatype.uuid()
+    const sut = makeSut(secret)
     const signSpy = jest.spyOn(jwt, 'sign')
+    const id = faker.datatype.uuid()
+    await sut.encrypt(id)
 
-    await sut.encrypt('any_value')
-
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+    expect(signSpy).toHaveBeenCalledWith({ id }, secret)
   })
 
   it('Should return a token on jwt sign success', async () => {
-    const sut = new JwtAdapter('secret')
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => 'valid_token')
+    const sut = makeSut()
+    const token = faker.datatype.uuid()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => token)
 
-    const accessToken = await sut.encrypt('any_value')
+    const accessToken = await sut.encrypt(faker.datatype.uuid())
 
-    expect(accessToken).toBe('valid_token')
+    expect(accessToken).toBe(token)
   })
 
   it('Should throw if jwt sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
 
-    const promise = sut.encrypt('any_value')
+    const promise = sut.encrypt(faker.datatype.uuid())
 
     await expect(promise).rejects.toThrow()
   })
