05d12290def85407b351facc86087bed49def636
Renato Mendes <mendeslrenato@gmail.com>
2022-12-08 23:51:47 -0300

    test: ensure login route returns 401 with invalid credentials

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
