104f50cca25f445b423f8684933999055c367434
Renato Mendes <mendeslrenato@gmail.com>
2022-12-13 23:26:45 -0300

    test: ensure JwtAdapter returns a decrypted token on verify success

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -3,7 +3,9 @@ import { JwtAdapter } from '@/infra/criptography/jwt-adapter'
 import { faker } from '@faker-js/faker'
 
 const token = faker.datatype.uuid()
+const secret = faker.datatype.uuid()
 const decryptedToken = faker.datatype.string()
+
 jest.mock('jsonwebtoken', () => ({
   sign (): string {
     return token
@@ -21,7 +23,6 @@ const makeSut = (secret = faker.datatype.uuid()): JwtAdapter => {
 describe('Jwt Adapter', () => {
   describe('sign()', () => {
     it('Should call sign with correct values', async () => {
-      const secret = faker.datatype.uuid()
       const sut = makeSut(secret)
       const signSpy = jest.spyOn(jwt, 'sign')
       const id = faker.datatype.uuid()
@@ -50,12 +51,19 @@ describe('Jwt Adapter', () => {
 
   describe('verify()', () => {
     it('Should call verify with correct values', async () => {
-      const secret = faker.datatype.uuid()
       const sut = makeSut(secret)
       const verifySpy = jest.spyOn(jwt, 'verify')
       await sut.decrypt(token)
 
       expect(verifySpy).toHaveBeenCalledWith(token, secret)
+    })
+
+    it('Should return a value on verify success', async () => {
+      const sut = makeSut()
+
+      const value = await sut.decrypt(token)
+
+      expect(value).toBe(decryptedToken)
     })
   })
 })
