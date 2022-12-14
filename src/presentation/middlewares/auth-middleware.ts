cae3ea6e5993630347adea4e1f1aa13a58f6a5eb
Renato Mendes <mendeslrenato@gmail.com>
2022-12-13 21:08:09 -0300

    feat: ensure AuthMiddleware returns server error if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,5 +1,5 @@
 import { AccessDeniedError } from '@/presentation/errors'
-import { forbidden, ok } from '@/presentation/helpers/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http-helper'
 import { LoadAccountByToken, HttpRequest, HttpResponse, Middleware } from '@/presentation/middlewares/middlewares-protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -8,12 +8,16 @@ export class AuthMiddleware implements Middleware {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken)
-      if (account) return ok({ accountId: account.id })
-    }
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
+        if (account) return ok({ accountId: account.id })
+      }
 
-    return forbidden(new AccessDeniedError())
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
