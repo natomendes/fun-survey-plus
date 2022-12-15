7d4092be0d8e93bb41afe634f3c857ad00537ac5
Renato Mendes <mendeslrenato@gmail.com>
2022-12-14 22:09:31 -0300

    feat: ensure LoadSurveysController returns server error if LoadSurveys usecase throws

--- src/presentation/controllers/load-surveys-controller.ts
+++ src/presentation/controllers/load-surveys-controller.ts
@@ -1,12 +1,16 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from '@/presentation/controllers/controllers-protocols'
-import { ok } from '@/presentation/helpers/http-helper'
+import { ok, serverError } from '@/presentation/helpers/http-helper'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveysList = await this.loadSurveys.load()
+    try {
+      const surveysList = await this.loadSurveys.load()
 
-    return ok(surveysList)
+      return ok(surveysList)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
