443a30ce70417c20126e59b6ac3f7fb80130700b
Renato Mendes <mendeslrenato@gmail.com>
2022-12-14 23:22:18 -0300

    test: ensure load surveys route returns forbidden if no access token is provided

--- tests/main/routes/survey-routes.test.ts
+++ tests/main/routes/survey-routes.test.ts
@@ -65,4 +65,12 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    it('Should return 403 on load surveys without access token', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+  })
 })
