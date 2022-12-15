19767bf7a640c13eecfad405ca982c3f6fcaffc1
Renato Mendes <mendeslrenato@gmail.com>
2022-12-15 10:59:49 -0300

    refactor: add Omit to usecases models to simplify code

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,8 +1,6 @@
-export type AddAccountModel = {
-  name: string
-  email: string
-  password: string
-}
+import { AccountModel } from '@/domain/models'
+
+export type AddAccountModel = Omit<AccountModel, 'id'>
 
 export interface AddAccount {
   add: (account: AddAccountModel) => Promise<boolean>
