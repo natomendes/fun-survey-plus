c62543bf9039c23fd095fc0e92e9e3d766f99587
Renato Mendes <mendeslrenato@gmail.com>
2022-12-14 22:44:03 -0300

    feat: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/usecases/db-load-surveys.ts
+++ src/data/usecases/db-load-surveys.ts
@@ -4,8 +4,6 @@ export class DbLoadSurveys implements LoadSurveys {
   constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
 
   async load (): Promise<SurveyModel[]> {
-    await this.loadSurveysRepository.loadAll()
-
-    return null
+    return await this.loadSurveysRepository.loadAll()
   }
 }
