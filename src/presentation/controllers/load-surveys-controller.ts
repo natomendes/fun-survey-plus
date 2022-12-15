0fc97e8e572a25b5bccc188da7258d240409c59a
Renato Mendes <mendeslrenato@gmail.com>
2022-12-14 22:05:55 -0300

    feat: ensure LoadSurveysController returns ok and the surveys list on success

--- src/presentation/controllers/load-surveys-controller.ts
+++ src/presentation/controllers/load-surveys-controller.ts
@@ -1,10 +1,12 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from '@/presentation/controllers/controllers-protocols'
+import { ok } from '@/presentation/helpers/http-helper'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const surveysList = await this.loadSurveys.load()
+
+    return ok(surveysList)
   }
 }
