59f01ba0cdeb9314f0c43c185a9ca5ee1361dc6e
Renato Mendes <mendeslrenato@gmail.com>
2022-12-20 18:39:57 -0300

    test: ensure load survey result route returns forbidden if access token is invalid

--- tests/main/routes/survey-result-routes.test.ts
+++ tests/main/routes/survey-result-routes.test.ts
@@ -47,4 +47,12 @@ describe('Survey Routes', () => {
         .expect(200)
     })
   })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    it('Should return 403 on load survey result without access token', async () => {
+      await request(app)
+        .get('/api/surveys/any_id/results')
+        .expect(403)
+    })
+  })
 })
