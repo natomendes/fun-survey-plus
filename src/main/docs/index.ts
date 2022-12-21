1da9d691f1f5a6ed41fd4e854faff063c8e2473f
Renato Mendes <mendeslrenato@gmail.com>
2022-12-21 02:01:11 -0300

    docs: update documentation

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -6,12 +6,20 @@ export default {
   openapi: '3.0.0',
   info: {
     title: 'Fun Survey Plus',
-    description: 'Fun Survey PLus, Node API using TDD, Clean Architecture and Typescript',
-    version: '1.0.0'
+    description: 'Fun Survey PLus is an open source Node API using TDD, Clean Architecture and Typescript',
+    version: '1.1.0',
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    },
+    contact: {
+      name: 'Renato Mendes (Author)',
+      url: 'https://www.linkedin.com/in/renatolmendes/'
+    }
   },
-  license: {
-    name: 'GPL-3.0-or-later',
-    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+  externalDocs: {
+    description: 'Visit the project repository.',
+    url: 'https://github.com/natomendes/fun-survey-plus'
   },
   servers: [{
     url: 'https://surveyapi.up.railway.app/api'
